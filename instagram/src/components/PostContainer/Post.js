7db2271ceb568795e4a669cefd00fb9c97c08228
ddot727 <ddot727@gmail.com>
2019-02-20 18:13:56 -0500

    forgot to import prop types

--- instagram/src/components/PostContainer/Post.js
+++ instagram/src/components/PostContainer/Post.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import CommentSection from '../CommentSection/CommentSection';
 import PostHeader from './PostHeader';
 import './PostContainer.css';
